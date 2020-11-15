eb34333760ae0aad39bafc5a61d784c856324c61
sunwj326 <sunwj326@hotmail.com>
2020-11-15 23:26:47 +0800

    rename httpEntity example class

--- javaframework/springmvc/05SpringMVC的基本使用4/springmvc_json/src/main/java/com/mashibing/controller/EntityController.java
+++ javaframework/springmvc/05SpringMVC的基本使用4/springmvc_json/src/main/java/com/mashibing/controller/EntityController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class EntiryController {
+public class EntityController {
 
     @RequestMapping("test")
     public String test(HttpEntity<String> httpEntity){
